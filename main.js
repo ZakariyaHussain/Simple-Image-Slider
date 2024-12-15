8c96446b68c3bd70a8c2b79722d3434303c8976b
ZakariyaHussain <168270318+ZakariyaHussain@users.noreply.github.com>
2024-12-15 18:51:42 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 
 
-var photoes = ["image/1.jpg", "image/2.jpg", "image/3.jpg"];
+var photoes = ["image/battery-charging.jpg", "image/battery-full.jpg", "image/battery-low.jpg"];
 var image  = document.querySelector("#img");
 var count = 0;
 function next(){
     count++;
     if(count >= photoes.length){
         count = 0;
+    }else{
+        image.src = photoes[count];
     }
     image.src = photoes[count];
-}
 
 function pre(){
     count--;
     if(count < 0){
         count = 2;
+    }else{
+        image.src = photoes[count];
     }
-    image.src = photoes[count]
-}
+    image.src = photoes[count];
